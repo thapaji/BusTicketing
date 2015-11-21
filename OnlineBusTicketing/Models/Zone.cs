8c910f95232daa5729469e71e8abe8870f332f92
amrit achhami <amrit achhami>
2015-11-21 11:07:48 +0545

    zone model created

---
+++ OnlineBusTicketing/Models/Zone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineBusTicketing.Models
+{
+    public class Zone
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+
+    }
+}
