8e8e4646c3c4038238533eed3ee7103206f288c1
thapaji <mail.thapasujan@gmail.com>
2015-11-26 08:34:39 +0545

    User Management session loaded and login done

--- OnlineBusTicketing/Controllers/AccountController.cs
+++ OnlineBusTicketing/Controllers/AccountController.cs
@@ -10,6 +10,9 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using OnlineBusTicketing.Models;
 using OnlineBusTicketing.Models.DAL;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using OnlineBusTicketing.Utilities;
 
 namespace OnlineBusTicketing.Controllers
 {
@@ -30,14 +33,64 @@ namespace OnlineBusTicketing.Controllers
             {
                 context.User.Add(user);
                 context.SaveChanges();
-                return View("Login");
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp.live.com");
+
+                    mail.From = new MailAddress("bus_project@outlook.com");
+                    mail.To.Add(user.Email);
+                    mail.Subject = "Verification Code";
+                    mail.Body = "Your Verification code is : " + user.VerificationCode;
+
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("bus_project@outlook.com", "onlineBusTicketing");
+                    SmtpServer.EnableSsl = true;
+
+                    SmtpServer.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                }
+                user.VerificationCode = 0;
+                Verify(user);
             }
             return View();
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
-            return View();
+            return View(new User());
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                string pass = Utility.GetMd5Hash(md5Hash, model.Password);
+                var user = (from s in context.User
+                            where s.Username == model.Username
+                            where s.Password == pass
+                            select s).FirstOrDefault();
+
+                if (user != null)
+                {
+                    Session["user"] = user;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    model.Password = "";
+                    return View(model);
+                }
+            }
+        }
+
+        public ActionResult Verify(User user)
+        {
+            return View(user);
         }
     }
 }
